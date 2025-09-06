5b133fbd2d2a2417781595eebc01087f9fbcf552
Jon Arve Ovesen <jonovesen@gmail.com>
2025-09-06 05:37:21 +0200

    Create test_orchestrator.py

---
+++ pad/tests/test_orchestrator.py
@@ -0,0 +1,9 @@
+"""
+Tester for OrchestratorAgent.
+"""
+from pad.orchestrator import OrchestratorAgent
+
+def test_plan_returns_input():
+    orchestrator = OrchestratorAgent()
+    inp = "lag en funksjon som skriver ut hei"
+    assert orchestrator.plan(inp) == inp
