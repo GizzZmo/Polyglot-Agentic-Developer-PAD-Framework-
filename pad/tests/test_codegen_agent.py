e85fd55c093c42cdab5e1535d20c01514ca0bc9a
Jon Arve Ovesen <jonovesen@gmail.com>
2025-09-06 05:38:34 +0200

    Add unit test for CodeGenAgent's code generation

---
+++ pad/tests/test_codegen_agent.py
@@ -0,0 +1,11 @@
+"""
+Tester for CodeGenAgent.
+"""
+from pad.codegen_agent import CodeGenAgent
+from pad.context_agent import ContextAgent
+
+def test_generate_code_includes_task():
+    agent = CodeGenAgent()
+    context = ContextAgent()
+    code = agent.generate_code("skriv ut hei", context)
+    assert "skriv ut hei" in code
